0f0b366ead948ebc2fa7ff09e7f2bf21ff07b291
ACarr33 <aidancarraretto@gmail.com>
2024-02-03 13:45:51 -0500

    Install MUI, begin app frontend

--- cryptogov/src/App.js
+++ cryptogov/src/App.js
@@ -1,22 +1,16 @@
+//Imports
 import './App.css';
+
+import { useState, useEffect } from "react";
+import {Box, Container, Toolbar} from "@mui/system";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container className="App">
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'auto auto'}}>
+
+        </Box>
+      </Container>
   );
 }
 
