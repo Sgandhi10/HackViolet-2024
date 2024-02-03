8a08b21a8b05ad7e3b5e602ac34d508bd7de5876
ACarr33 <aidancarraretto@gmail.com>
2024-02-03 16:35:42 -0500

    change dummy data from function to const

--- cryptogov/src/MainPage.js
+++ cryptogov/src/MainPage.js
@@ -5,42 +5,40 @@ import ContractDetails from './ContractDetails.js';
 import {Box, Container} from "@mui/system";
 import {Toolbar, Button, Typography, AppBar} from "@mui/material";
 
+//dmumy
+const dummyData = {
+    "contracts": [{
+        "name": "gisaflrbr",
+        "description": "descrrepi",
+        "status": "working",
+        "cid": 1,
+        "hoursWorked": 23,
+        "cost": 30004
+    },
+        {
+            "name": "argagaga",
+            "description": "r",
+            "status": "broken",
+            "cid": 30,
+            "hoursWorked": 1,
+            "cost": 3
+        },
+        {
+            "name": "dumy 3",
+            "description": "idiot",
+            "status": "kaput",
+            "cid": 3,
+            "hoursWorked": 10000,
+            "cost": 50000000000000
+        }]
+};
+
 export default function MainPage() {
 
     const [selectedContract, setSelectedContract] = useState(null); //Currently selected/clicked contract box
 
     //Called when logout button is pushed; logs out user and returns to logout page
     function handleLogOut(event){
-    }
-
-    //dmumy
-    function dummyData() {
-        return {
-            "contracts": [{
-                "name": "gisaflrbr",
-                "description": "descrrepi",
-                "status": "working",
-                "cid": 1,
-                "hoursWorked": 23,
-                "cost": 30004
-            },
-                {
-                    "name": "argagaga",
-                    "description": "r",
-                    "status": "broken",
-                    "cid": 30,
-                    "hoursWorked": 1,
-                    "cost": 3
-                },
-                {
-                    "name": "dumy 3",
-                    "description": "idiot",
-                    "status": "kaput",
-                    "cid": 3,
-                    "hoursWorked": 10000,
-                    "cost": 50000000000000
-                }]
-        };
     }
 
     function handleContractClick(event){
@@ -63,9 +61,9 @@ export default function MainPage() {
     </Box>
     <Box sx={{ display: 'grid', gridTemplateColumns: 'auto auto'}}>
         <ContractList
-
             sx={{bgcolor: 'white', width: '100%', height: '100%'}}
             clickHandler = {handleContractClick}
+            dummyData = {dummyData}
             />
 
             <ContractDetails
