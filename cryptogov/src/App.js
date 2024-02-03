56d505ec6fe2ea14afff87ec587bf23c8de742de
ACarr33 <aidancarraretto@gmail.com>
2024-02-03 14:34:38 -0500

    Appbar implementation progress

--- cryptogov/src/App.js
+++ cryptogov/src/App.js
@@ -7,16 +7,25 @@ import { useState, useEffect } from "react";
 import {Box, Container} from "@mui/system";
 import {Toolbar, Button, Typography, AppBar} from "@mui/material";
 
+//cont selectedContract = useState()
+
 function App() {
+
+    //Called when logout button is pushed; logs out user and returns to logout page
+    function handleLogOut(event){
+    }
+
   return (
       <Container className="App">
           <Box>
               <AppBar position="static">
                   <Toolbar>
-                      <Typography sx={{ fontFamily: 'arial'}}>
+                      <Typography sx={{ fontFamily: 'arial', mr: 2, flexGrow: 1 }}>
                           CryptoGov
                       </Typography>
-
+                      <Button color = "inherit" onClick = {handleLogOut} sx={{ fontFamily: 'arial'}}>
+                          Log out
+                      </Button>
                   </Toolbar>
               </AppBar>
           </Box>
