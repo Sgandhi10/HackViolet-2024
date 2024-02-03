51047b606d6d725f214375b7c2c92784a500e5a7
ACarr33 <aidancarraretto@gmail.com>
2024-02-03 14:06:02 -0500

    Create appbar and contractbox/list frontend components

--- cryptogov/src/App.js
+++ cryptogov/src/App.js
@@ -1,17 +1,39 @@
 //Imports
 import './App.css';
+import ContractList from './ContractList.js'
+import ContractDetails from './ContractDetails.js'
 
 import { useState, useEffect } from "react";
-import {Box, Container, Toolbar} from "@mui/system";
+import {Box, Container} from "@mui/system";
+import {Toolbar, Button, Typography, AppBar} from "@mui/material";
 
 function App() {
   return (
       <Container className="App">
-        <Box sx={{ display: 'grid', gridTemplateColumns: 'auto auto'}}>
+          <Box>
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography sx={{ fontFamily: 'arial'}}>
+                          CryptoGov
+                      </Typography>
 
-        </Box>
+                  </Toolbar>
+              </AppBar>
+          </Box>
+          <Box sx={{ display: 'grid', gridTemplateColumns: 'auto auto'}}>
+
+          </Box>
       </Container>
   );
 }
 
+/*
+              <ContractList
+
+              />
+
+              <ContractDetails
+
+              />
+*/
 export default App;
