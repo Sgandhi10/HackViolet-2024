d09f0d259a6a2121a0d105c08fef38cc6ef4ebb4
ACarr33 <aidancarraretto@gmail.com>
2024-02-03 18:37:19 -0500

    Change onclick to onmouseover

--- cryptogov/src/ContractListItem.js
+++ cryptogov/src/ContractListItem.js
@@ -2,7 +2,7 @@ import {Button} from "@mui/material";
 
 function ContractListItem(props){
     const dataIndex = props.indexNum;
-    return <Button sx={{color: "black", backgroundColor: "#ffffff", border: 2, width:"100%"}} size = "large" onClick = {props.clickHandler} id = {dataIndex} >
+    return <Button sx={{color: "black", backgroundColor: "#ffffff", border: 2, width:"100%"}} size = "large" onMouseOver = {props.clickHandler} id = {dataIndex} >
         {props.contract.name + "  ID: " + props.contract.cid}
     </Button>
 }
