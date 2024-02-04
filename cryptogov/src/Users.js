34112666bf6daed9f43ab66483dafd975babea40
aldeng21 <aldengyening@gmail.com>
2024-02-04 11:43:45 -0500

    Added Verified Users and Authentication

---
+++ cryptogov/src/Users.js
@@ -0,0 +1,24 @@
+//Constant Dictionary of users 
+
+
+
+
+
+export const VERIFIED_USERS = {
+    "users": [{
+        "name": "Jake Long",
+        "pubkey": "5baf607c4ce515fd",
+        "privkey": "350293395012608f"
+    },
+    {
+        "name": "Jenna Norton",
+        "pubkey": "57319eeae31b392d",
+        "privkey": "d34959cba9266625"
+    },
+    {
+        "name": "Matt Melvin",
+        "pubkey": "eb30e18d7686cdb4",
+        "privkey": "819574f6307bea6d"
+        
+    }]
+};
