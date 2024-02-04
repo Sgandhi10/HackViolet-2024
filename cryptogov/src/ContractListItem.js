1d1079ee5f9c5d11b0823779a9e83edeb7737c8e
Gauss <hedpub@protonmail.com>
2024-02-04 10:04:51 -0500

    ContractListItem formatting change

--- cryptogov/src/ContractListItem.js
+++ cryptogov/src/ContractListItem.js
@@ -3,7 +3,7 @@ import {Button} from "@mui/material";
 function ContractListItem(props){
     const dataIndex = props.indexNum;
     return <Button sx={{color: "black", backgroundColor: "#ffffff", border: 2, width:"100%", '&:hover' : {backgroundColor: "#c9c9c9"}}} size = "large" onMouseOver = {props.clickHandler} id = {dataIndex} >
-        {props.contract.name + "  ID: " + props.contract.cid}
+        {props.contract.name + " (" + props.contract.cid + ")"}
     </Button>
 }
 
