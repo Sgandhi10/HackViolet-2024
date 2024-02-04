403d3d12587ca4530e212c3c624e48fc5bce95ae
Gauss <hedpub@protonmail.com>
2024-02-04 10:15:08 -0500

    ContractListItems are onClick again

--- cryptogov/src/ContractListItem.js
+++ cryptogov/src/ContractListItem.js
@@ -2,7 +2,7 @@ import {Button} from "@mui/material";
 
 function ContractListItem(props){
     const dataIndex = props.indexNum;
-    return <Button sx={{color: "black", backgroundColor: "#ffffff", border: 2, width:"100%", '&:hover' : {backgroundColor: "#c9c9c9"}}} size = "large" onMouseOver = {props.clickHandler} id = {dataIndex} >
+    return <Button sx={{color: "black", backgroundColor: "#ffffff", border: 2, width:"100%", '&:hover' : {backgroundColor: "#c9c9c9"}}} size = "large" onClick = {props.clickHandler} id = {dataIndex} >
         {props.contract.name + " (" + props.contract.cid + ")"}
     </Button>
 }
